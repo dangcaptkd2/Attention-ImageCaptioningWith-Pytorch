90df7c99ac4497da391a73f5cd25cff08c3dd0ce
dangcaptkd2 <62542082+dangcaptkd@users.noreply.github.com>
2021-04-16 22:06:49 +0700

    turn path into flick8k dataset

--- create_input_files.py
+++ create_input_files.py
@@ -2,11 +2,11 @@ from utils import create_input_files
 
 if __name__ == '__main__':
     # Create input files (along with word map)
-    create_input_files(dataset='coco',
-                       karpathy_json_path='../caption data/dataset_coco.json',
-                       image_folder='/media/ssd/caption data/',
+    create_input_files(dataset='flickr8k',
+                       karpathy_json_path='/content/caption data/dataset_flickr8k.json',
+                       image_folder='/content/caption data/Images',
                        captions_per_image=5,
                        min_word_freq=5,
-                       output_folder='/media/ssd/caption data/',
+                       output_folder='/content/caption data/',
                        max_len=50)
-© 2021 GitHub, Inc.
+
